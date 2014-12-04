d4c766fcc48957af2830b8dac1e185f3a1d23ca1
ayebear <ayebear@gmail.com>
2014-12-04 14:06:53 -0500

    Fixed quote escaping with a function, but made the code a lot longer

--- relay.c
+++ relay.c
@@ -1,34 +1,68 @@
 #include <stdio.h>
-#define S(x) "%c%c%s%c%c%c",9,34,x,34,44,10
-#define print_with_quotes(s) printf(S(s));
+#define print_with_quotes(s) printf("%c%c%s%c,%c",9,34,s,34,10);
+
+void print_with_escape(const char* s)
+{
+	putchar(9);
+	putchar(34);
+	while (*s) {
+		if (*s == 34) {
+			putchar(92);
+		}
+		putchar(*s);
+		++s;
+	}
+	putchar(34);
+	putchar(44);
+	putchar(10);
+}
 
 int main()
-{	
+{
 	unsigned i;
 	const char *data[] = {
 	"#include <stdio.h>",
-	"#define S(x) \"%c%c%s%c%c%c\",9,34,x,34,44,10",
-	"#define print_with_quotes(s) printf(S(s));",
+	"#define print_with_quotes(s) printf(\"%c%c%s%c,%c\",9,34,s,34,10);",
+	"",
+	"void print_with_escape(const char* s)",
+	"{",
+	"	putchar(9);",
+	"	putchar(34);",
+	"	while (*s) {",
+	"		if (*s == 34) {",
+	"			putchar(92);",
+	"		}",
+	"		putchar(*s);",
+	"		++s;",
+	"	}",
+	"	putchar(34);",
+	"	putchar(44);",
+	"	putchar(10);",
+	"}",
 	"",
 	"int main()",
 	"{",
 	"	unsigned i;",
 	"	const char *data[] = {",
 	"	NULL};",
-	"	for(i = 0; i < 8; i++)",
+	"	for (i = 0; i < 23; ++i)",
 	"		puts(data[i]);",
-	"	for(i = 0; data[i] != NULL; i++)",
+	"	print_with_quotes(data[0]);",
+	"	print_with_escape(data[1]);",
+	"	for (i = 2; data[i] != NULL; ++i)",
 	"		print_with_quotes(data[i]);",
-	"	for(i = 8; data[i] != NULL; i++)",
+	"	for (i = 23; data[i] != NULL; ++i)",
 	"		puts(data[i]);",
 	"	return 0;",
 	"}",
 	NULL};
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 23; ++i)
 		puts(data[i]);
-	for(i = 0; data[i] != NULL; i++)
+	print_with_quotes(data[0]);
+	print_with_escape(data[1]);
+	for (i = 2; data[i] != NULL; ++i)
 		print_with_quotes(data[i]);
-	for(i = 8; data[i] != NULL; i++)
+	for (i = 23; data[i] != NULL; ++i)
 		puts(data[i]);
 	return 0;
 }
